a39e111855c174c4ba70d4bb4b35ab727498bdff
rsgc-chien-g <student@MacBook-Pro-0681-GChien.local>
2016-01-16 20:59:02 -0500

    I started working in the command line that was provided. I forgot that I should have put it in to a new file. I used the original code as a base and made a lot of modification to it. The problem I’m having is that the code only works for 1 instrument combination. I am working on fixing that.

--- Transposer.playground/Contents.swift
+++ Transposer.playground/Contents.swift
@@ -4,23 +4,24 @@ import Cocoa
 
 var str = "Hello, playground"
 // Input Instrument
-var instrument1 = ["Trumpet/Tenor", "Alto", "Trombone", "Flute", "French Horn"]
+var instrument1 = ["Trumpet/Tenor/Clarinet/Flute", "Alto/Baritone", "Trombone", ]
 // Output Instrument
-var instrument2 = ["Trumpet/Tenor", "Alto", "Trombone", "Flute"]
+var instrument2 = ["Trumpet/Tenor/Clarinet/Flute", "Alto/Baritone", "Trombone", ]
 // Input Note
 var notes1: [String] = ["Bb", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"]
 // Input Note
 var notes2: [String] = ["Bb", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"]
 
 // Chosen Input instrument
-var inputInst: Int = 2
+var inputInst: Int = 0
 // Chosen Output instrument
 var outputInst: Int = 1
 // Input note
 var inputNote: Int = 0
 
+
 notes2 
-// Alto -> Trumpet/Tenor
+// Alto/Baritone -> Trumpet/Tenor
 if (instrument1[inputInst] == instrument1[1] && instrument2[outputInst] == instrument2[0]){
     instrument1[inputInst]
     notes1[inputNote]
@@ -29,7 +30,7 @@ if (instrument1[inputInst] == instrument1[1] && instrument2[outputInst] == instr
     notes2[inputNote] = notes1[(inputNote+5)%12]
 }
 
-// Trumpet/Tenor -> Alto
+// Trumpet/Tenor -> Alto/Baritone
 if (instrument1[inputInst] == instrument1[0] && instrument2[outputInst] == instrument2[1]){
     instrument1[inputInst]
     notes1[inputNote]
@@ -56,7 +57,7 @@ if (instrument1[inputInst] == instrument1[2] && instrument2[outputInst] == instr
     notes2[inputNote] = notes1[(inputNote+2)%12]
 }
 
-// Alto -> Trombone
+// Alto/Baritone -> Trombone
 if (instrument1[inputInst] == instrument1[1] && instrument2[outputInst] == instrument2[2]){
     instrument1[inputInst]
     notes1[inputNote]
@@ -65,7 +66,7 @@ if (instrument1[inputInst] == instrument1[1] && instrument2[outputInst] == instr
     notes2[inputNote] = notes1[(inputNote+3)%12]
 }
 
-// Trombone -> Alto
+// Trombone -> Alto/Baritone
 if (instrument1[inputInst] == instrument1[2] && instrument2[outputInst] == instrument2[1]){
     instrument1[inputInst]
     notes1[inputNote]
@@ -73,3 +74,4 @@ if (instrument1[inputInst] == instrument1[2] && instrument2[outputInst] == instr
     instrument2[outputInst]
     notes2[inputNote] = notes1[(inputNote+9)%12]
 }
+
